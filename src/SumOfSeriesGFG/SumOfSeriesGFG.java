9229d14b99713edfc706384ee7f88d1cdd29a4cb
Mohd Danish <mddanish.cse@gmail.com>
2023-12-25 15:01:16 +0530

    Initial commit

---
+++ src/SumOfSeriesGFG/SumOfSeriesGFG.java
@@ -0,0 +1,12 @@
+//Sum of Series
+//Link: https://www.geeksforgeeks.org/problems/sum-of-series2811/1
+
+package SumOfSeriesGFG;
+
+class Solution {
+    // function to return sum of  1, 2, ... n
+    long seriesSum(int n) {
+        // code here
+        return (long) n*(n+1)/2;
+    }
+}
