ec8ff535899cf071859e684edab5cbff586417c3
Mohd Danish <mddanish.cse@gmail.com>
2023-12-25 15:23:47 +0530

    Initial commit

---
+++ src/PrintElementsOfArrayGFG/PrintElementsOfArrayGFG.java
@@ -0,0 +1,13 @@
+//Print Elements of Array
+//Link: https://www.geeksforgeeks.org/problems/print-elements-of-array4910/1
+
+package PrintElementsOfArrayGFG;
+
+class Solution {
+    //Just print the space seperated array elements
+    void printArray(int arr[], int n) {
+        //   code here
+        for(int i=0;i<n;i++)
+            System.out.print(arr[i]+" ");
+    }
+}
